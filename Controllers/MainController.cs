f1aa5c8583c051952cb50254657b599d7406d395
Ilakiyasaravanan <58771810+Ilakiyasaravanan@users.noreply.github.com>
2020-02-20 09:27:04 +0530

    Add project files.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JobPortal_Entity;
+using JobPortal_DAL;
+
+namespace JobPortal_MVCAssignment.Controllers
+{
+    public class MainController : Controller
+    {
+       [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult SignUp()
+        //{
+        //    JobPersonDetails jobdetails = new JobPersonDetails();
+        //    jobdetails.Add(jobdetails);
+        //    ViewBag["Message"] = "Added";
+        //    return View();
+            
+        //}
+    }
+}
